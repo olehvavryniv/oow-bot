04b3cdff760cabe1965bccf365531fe66be78a33
oleh <oleh02@gmail.com>
2020-06-05 15:03:38 +0300

    remove useless repond

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -3,10 +3,6 @@
 require './services/steam-service.rb'
 
 class MainController < Telegram::Bot::UpdatesController
-  def message(message)
-    respond_with :message, message
-  end
-
   def steam!(*)
     statuses = SteamService.new.oow_statuses.map { |d| "#{d[:name]} - #{d[:status]}" }.join("\n")
     respond_with :message, text: statuses
