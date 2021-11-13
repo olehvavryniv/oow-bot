b0ca8137130008adbd7165241b193357a044f951
Nick Ovchynnikov <46745559+NickOvc@users.noreply.github.com>
2021-11-13 17:30:39 +0200

    create constant SHOSHES (#10)
    * create constant SHOSHES
    
    * move SHOSHES to contstants, require constants in main controller

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 require './services/steam-service.rb'
+require './constants.rb'
 
 class MainController < Telegram::Bot::UpdatesController
+
   def steam!(*)
     statuses = SteamService.new.oow_statuses.map { |d| "#{d[:name]} - #{d[:status]}" }.join("\n")
     respond_with :message, text: statuses
   end
 
   def shosh!(*)
-    respond_with :message, text: 'az ziz'
+    respond_with :message, text: SHOSHES.sample
   end
 end
