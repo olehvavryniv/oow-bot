3f96e235e67cd1cf49045383a2600620debecaae
Nick Ovchynnikov <o.mykola@fasterthanlight.me>
2021-11-13 15:28:13 +0200

    move SHOSHES to contstants, require constants in main controller

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
 
 require './services/steam-service.rb'
+require './constants.rb'
 
 class MainController < Telegram::Bot::UpdatesController
-
-  SHOSHES = [
-    'мне чуть стекла не разбились',
-    'ну отец то ясно цего 2 труп по скайпу будет сидеть',
-    'я это сегодня черный член',
-    'у меня теперь 4 глаза не могут фокусироваться',
-    'Случайно локтем комп вырубил',
-    'Mor 6bl u 4To To JIy4IIIe cni3DaHyYb',
-    'продался отписка контент не тот'
-  ]
 
   def steam!(*)
     statuses = SteamService.new.oow_statuses.map { |d| "#{d[:name]} - #{d[:status]}" }.join("\n")
