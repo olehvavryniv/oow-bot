a04fea81a259da50dad9347c5da504ea852f7aac
Nick Ovchynnikov <46745559+NickOvc@users.noreply.github.com>
2023-10-04 21:53:45 +0300

    add function, inferno+overpass positions

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -7,6 +7,7 @@ require './services/last-games-sevice.rb'
 require './constants.rb'
 
 class MainController < Telegram::Bot::UpdatesController
+  @@message = nil
   include CallbackQueryContext
 
   def steam!(*)
@@ -29,6 +30,45 @@ class MainController < Telegram::Bot::UpdatesController
     ShotamService.instance.last_messages[message['result']['chat']['id']] = message['result']['message_id']
   end
 
+  def position!(*)
+    position_key = CS_POSITIONS.to_a.sample.first
+    position_names = CS_POSITIONS[position_key].values.join(", ")
+    respond_with :photo, photo: File.open(position_key), caption: position_names
+  end
+
+  def random_position!(*)
+    @@message = nil
+    position_key = CS_POSITIONS.to_a.sample.first
+    correct_names = CS_POSITIONS[position_key].values.first
+    map = CS_POSITIONS[position_key].keys[0]
+    given_map_positions = CS_POSITIONS.select {|k, v| v.keys.first == map }
+    #remove correct name from the list
+    given_map_positions.delete(position_key)
+    wrong_names = []
+    given_map_positions.values.each do |i|
+      wrong_names << i[map].flatten
+    end
+    correct_answer = correct_names.sample
+    formatted_array =  (wrong_names.flatten.first(2) << correct_answer).shuffle
+
+    bot.send_photo(chat_id: chat['id'], photo: File.open(position_key), caption: "What is the name of this position?")
+    answers =
+      {
+        inline_keyboard: [
+          [
+            { text: "#{formatted_array[0]}", callback_data: "random_position:#{formatted_array[0]}/#{correct_answer}" },
+          ],
+          [
+            { text: "#{formatted_array[1]}", callback_data: "random_position:#{formatted_array[1]}/#{correct_answer}" },
+          ],
+          [
+            { text: "#{formatted_array[2]}", callback_data: "random_position:#{formatted_array[2]}/#{correct_answer}" },
+          ],
+        ]
+      }
+    respond_with :message, text: "Choose one:", reply_markup: answers
+  end
+
   def test!(*)
     puts chat['id']
   end
@@ -38,5 +78,15 @@ class MainController < Telegram::Bot::UpdatesController
     answer_callback_query('Ok')
   rescue Telegram::Bot::Error => e
     # pofig
+  end
+
+  def random_position_callback_query(answer, *)
+    # unreal kostilyaka
+    return if @@message
+    status = (answer.split('/').uniq.length == 1)
+    text = status ? "✅Correct! This position is called <b><i>#{answer.split('/').first}</i></b>" : "❌#{LOH_NAMES.sample.capitalize}! This position is called <b><i>#{answer.split('/').last}</i></b>"
+    @@message = bot.send_message(chat_id: chat['id'], text: text, parse_mode: "HTML")
+  rescue Telegram::Bot::Error => e
+    # ve
   end
 end
